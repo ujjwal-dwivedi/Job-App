a698d0660cee2c26006fc4b90f9cdb2ae7154b36
ujjwal6bitquasar <ujjwal264dwivedi@gmail.com>
2025-07-26 15:10:18 +0530

    Navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,31 @@
-import './App.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/shared/Navbar'
+import Home from './components/Home'
+import Login from './components/auth/Login'
+import Signup from './components/auth/Signup'
+
+
+const appRouter=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home/>
+  },
+  {
+    path:'/login',
+    element:<Login/>
+  },
+  {
+    path:'/signup',
+    element: <Signup/>
+  }
+])
+
 
 function App() {
   return (
     <>
-    <Navbar />
+    <RouterProvider router={appRouter}/>
     </>
   )
 }
